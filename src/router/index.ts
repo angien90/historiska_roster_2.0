0d9cc7ad5cb7c89ad8ea827a485a9f8b287710ac
angien90 <angelicanylander@hotmail.com>
2025-08-19 19:59:42 +0200

    Bytte tillbaka build och WebHistory

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory } from 'vue-router';
+import { createRouter, createWebHistory } from 'vue-router';
 
 const routes = [
   { path: "/", name: "HomePage", component: () => import("../views/HomePage.vue") },
@@ -20,7 +20,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory(),
   routes,
   scrollBehavior(to, from, savedPosition) {
     // Scrolla alltid till toppen
